ffebaff43fae9bb9feca9be418c92bfb610c1238
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-10 12:42:34 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import express, {Application} from "express"
-import cors from "cors"
-import morgan from "morgan"
-
-
+import express, { Application } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 export const appConfig = (app: Application) => {
-    app.use(express.json()).use(cors()).use(morgan("dev")
-)}
+  app.use(express.json()).use(cors()).use(morgan("dev"));
+};
--- src/config/environmentvarabiles.ts
+++ src/config/environmentvarabiles.ts
@@ -1,11 +1,10 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const environmentsvariables = {
-    PORT: process.env.PORT as string,
-    LOCAL_DATABASE: process.env.LOCAL_DATABASE as string
-}
+  PORT: process.env.PORT as string,
+  LOCAL_DATABASE: process.env.LOCAL_DATABASE as string,
+};
 
-
-export default environmentsvariables
+export default environmentsvariables;
