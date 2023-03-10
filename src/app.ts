173e6560e7ecde4399cec8bbd6664acab0eca088
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-10 13:13:43 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { erroHandeler } from "./middleware/errorHandler";
 
 export const appConfig = (app: Application) => {
   app.use(express.json()).use(cors()).use(morgan("dev"))
-  .all()
+  .all("*", (req: Request, res: Response, next: NextFunction) => {
+res.status(404).json({
+  message: `This route ${req.originalUrl} is not found`
+})
+  })
   // ErrorHandler
   .use(erroHandeler)
 };
